e233d1dcfc9f0fa404ac98de25b8bbae64f46954
I-am-Skboss <98142467+I-am-Skboss@users.noreply.github.com>
2023-09-13 18:40:22 +0000

    Connection database check 1

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 require('dotenv').config()
 require("./database/database.js")
 const express = require('express')
+const res = require('express/lib/response.js')
 
 const app = express()
+app.use(express.json())
 
 app.get("/", (req, res) =>{
     res.send("<h1>Server is working</h1>")
 })
+app.post("./register", (req, res)) =>{
+    try{
 
+    }catch(error){
+        console.loh(error);
+    }
+}
 
 module.exports = app
--- database/database.js
+++ database/database.js
@@ -3,12 +3,13 @@ const mongoose = require("mongoose")
 const {MONGODB_URL} = process.env
 
 exports.connect = () => {
+    console.log(typeof MONGODB_URL);
     mongoose.connect(MONGODB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     } )
     .then(
-        console.log(`DB Connected SUCCESSFYLLY`)
+        console.log(`DB Connected SUCCESSFuLLY`)
     )
     .catch((error) => {
         console.log(`DB connection FAILED`);
