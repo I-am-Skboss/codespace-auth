33afb894d88618afef8d875657ca27590df58498
I-am-Skboss <98142467+I-am-Skboss@users.noreply.github.com>
2023-10-03 17:34:44 +0000

    Updating link instead of server name

--- database/database.js
+++ database/database.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose")
 
 const {MONGODB_URL} = process.env
+MONGODB_URL == "mongodb+srv://Shivam_kushwaha:Shivam123@cluster0.rpkcw2e.mongodb.net/test"
 
 exports.connect = () => {
     // console.log(typeof MONGODB_URL);
-    mongoose.connect(MONGODB_URL, {
+    mongoose.connect("mongodb+srv://Shivam_kushwaha:Shivam123@cluster0.rpkcw2e.mongodb.net/test", {
         useNewUrlParser: true,
         useUnifiedTopology: true
     } )
