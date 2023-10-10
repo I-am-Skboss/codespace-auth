fcc6b890e9c78707cfe6b3b65359312c8cc7fb4a
I-am-Skboss <98142467+I-am-Skboss@users.noreply.github.com>
2023-10-10 17:06:17 +0000

    Database link updated

--- database/database.js
+++ database/database.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose")
-
+const express = require("express")
 const {MONGODB_URL} = process.env
-MONGODB_URL == "mongodb+srv://Shivam_kushwaha:Shivam123@cluster0.rpkcw2e.mongodb.net/test"
 
 exports.connect = () => {
+
+const app = express();
+app.use(express.json());
     // console.log(typeof MONGODB_URL);
-    mongoose.connect("mongodb+srv://Shivam_kushwaha:Shivam123@cluster0.rpkcw2e.mongodb.net/test", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
+    
+mongoose.connect(MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
     } )
-    .then(
-        console.log(`DB connected Successfully`)
+.then(
+    console.log(`DB connected Successfully`)
     )
-    .catch((error) => {
-        console.log(`DB connection FAILED`);
-        console.log(error);
-        process.exit(1)
-    })
+.catch((error) => {
+    console.log(`DB connection FAILED`);
+    console.log(error);
+    process.exit(1)
+})
 }
